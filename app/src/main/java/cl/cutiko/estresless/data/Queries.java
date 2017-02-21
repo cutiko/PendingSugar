820c08f0db2c305df03937568dc4c0e929650e18
cutiko <cutiko@gmail.com>
2017-02-20 21:52:29 -0300

    first commit

---
+++ app/src/main/java/cl/cutiko/estresless/data/Queries.java
@@ -0,0 +1,32 @@
+package cl.cutiko.estresless.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.cutiko.estresless.models.Pending;
+
+/**
+ * Created by cutiko on 15-02-17.
+ */
+
+public class Queries {
+
+    public List<Pending> notDone() {
+        List<Pending> pendings = new ArrayList<>();
+        List<Pending> pendingList = Pending.find(Pending.class, "done = 0");
+        if (pendingList != null && pendingList.size() > 0) {
+            pendings.addAll(pendingList);
+        }
+        return pendings;
+    }
+
+    public List<Pending> done() {
+        List<Pending> pendings = new ArrayList<>();
+        List<Pending> pendingList = Pending.find(Pending.class, "done = 1");
+        if (pendingList != null && pendingList.size() > 0) {
+            pendings.addAll(pendingList);
+        }
+        return pendings;
+    }
+
+}
